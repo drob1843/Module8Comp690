dea5dccad2d56ad4617f36b07d472b00cd1ccef1
drob1843 <94759314+drob1843@users.noreply.github.com>
2024-12-02 19:38:16 -0800

    Last

--- js/script-ems.js
+++ js/script-ems.js
@@ -2,12 +2,16 @@
 
 let employees = [
     [12345671, 'Diego', 1231, 'diego@email.com', 'Administrative'],
-    [12345672, 'Zak', 1232,'zak@email.com', 'Engineering'],
-    [12345673, 'Ruth', 1233, 'ruth@email.com','Sales'],
-    [12345674, 'Mary', 1234, 'mary@email.com','Quality Assurance'],
-    [12345675, 'Dan', 1235, 'danb@email.com','Marketing'],
+    [12345672, 'Zak', 1232, 'zak@email.com', 'Engineering'],
+    [12345673, 'Ruth', 1233, 'ruth@email.com', 'Sales'],
+    [12345674, 'Mary', 1234, 'mary@email.com', 'Quality Assurance'],
+    [12345675, 'Dan', 1235, 'dan@email.com', 'Marketing'],
 ]
 
+
+
+// let employeeCount = 0
+// document.getElementById('empCount').textContent = `(${employeeCount})`
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
@@ -21,27 +25,13 @@ if (storedEmployees) {
 // GET DOM ELEMENTS
 
 const $ = id => document.getElementById(id)
+const form = $('addForm')
+const empTable = $('empTable')
+
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
-function view(employees) {
-    let tbody = ''
-    employees.forEach(emp => {
-        tbody += `
-        <tr>
-            <td>${emp[0]}</td>
-            <td>${emp[1]}</td>
-            <td>${emp[2]}</td>
-            <td>${emp[3]}</td>
-            <td>
-                <button class="delete">Delete</button>
-            </td>
-            </tr>
-        `
-    })
-    $('empTable').querySelector('tbody').innerHTML = tbody
-}
 
-view(employees)
+buildGrid()
 
 // ADD EMPLOYEE
 
@@ -57,25 +47,22 @@ form.addEventListener('submit', (e) => {
     let email = document.getElementById('email').value
     let department = document.getElementById('department').value
 
-
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
 
     let addEmp = [id, name, extension, email, department]
 
+
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
 
-    let employeesNewLength = employees.push(addEmp)
+    employees.push(addEmp)
+    // console.log(employees)
 
     // BUILD THE GRID
 
-    view(employees)
+    buildGrid()
 
-
-
-// RESET THE FORM
-
-
-resetForm()
+    // RESET THE FORM
+    resetForm()
 });
 const resetForm = () => {
     form.reset()
@@ -85,33 +72,65 @@ const resetForm = () => {
 }
 
 
-
-
 // DELETE EMPLOYEE
 empTable.addEventListener('click', (e) => {
     // CONFIRM THE DELETE
+    if (!e.target.classList.contains('delete')) return
+    if (!confirm('Delete employee?')) return
+
 
     // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
 
+    const rowIndex = e.target.parentNode.parentNode.rowIndex - 1
+
     // REMOVE EMPLOYEE FROM ARRAY
+
+    employees.splice(rowIndex, 1)
+
 
     // BUILD THE GRID
 
+    buildGrid()
+
 });
+
+
+
 
 // BUILD THE EMPLOYEES GRID
 function buildGrid() {
     // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
 
+    $('empTable').querySelector('tbody').innerHTML = ''
+
     // REBUILD THE TBODY FROM SCRATCH
 
-    // LOOP THROUGH THE ARRAY OF EMPLOYEES
-    // REBUILDING THE ROW STRUCTURE
+    let tbodyContent = ''
+    for (let emp of employees) {
+        // LOOP THROUGH THE ARRAY OF EMPLOYEES
+        // REBUILDING THE ROW STRUCTURE
 
+        tbodyContent += `
+        <tr>
+            <td>${emp[0]}</td>
+            <td>${emp[1]}</td>
+            <td>${emp[2]}</td>
+            <td>${emp[3]}</td>
+            <td>
+                <button class="delete">Delete</button>
+            </td>
+        </tr>
+        `
+    }
     // BIND THE TBODY TO THE EMPLOYEE TABLE
+
+    $('empTable').querySelector('tbody').innerHTML = tbodyContent
 
     // UPDATE EMPLOYEE COUNT
 
+    $('empCount').textContent = `(${employees.length})`
+
     // STORE THE ARRAY IN STORAGE
+    localStorage.setItem('employees', JSON.stringify(employees))
 
 };
